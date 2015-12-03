3cee7c8504080617905f78fa834d6a7f37813cf5
su033503 <su033503@naver.com>
2015-12-03 11:51:38 +0900

    sqlite1

---
+++ sglite/src/main/java/com/example/com/sglite/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.com.sglite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    myDBHelper myHelper;
+    EditText edtName, edtCount, edtNameResult, edtCountResult;
+    Button btnInit, btnInsert, btnSelect, btnModify, btnRemove;
+    SQLiteDatabase sqlDB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("가수 그룹 관리 DB");
+
+        edtName = (EditText)findViewById(R.id.edtName);
+        edtCount = (EditText)findViewById(R.id.edtCount);
+        edtNameResult = (EditText)findViewById(R.id.edtNameResult);
+        edtCountResult = (EditText)findViewById(R.id.edtCountResult);
+        btnInit = (Button)findViewById(R.id.btnInit);
+        btnInsert = (Button)findViewById(R.id.btnInsert);
+        btnSelect = (Button)findViewById(R.id.btnSelect);
+        btnModify = (Button)findViewById(R.id.btnModify);
+        btnRemove = (Button)findViewById(R.id.btnRemove);
+
+        myHelper = new myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB,1,2);
+                sqlDB.close();
+            }
+        });
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO groupTBL VALUES ( '"+edtName.getText().toString()+"' , "+edtCount.getText().toString()+");");
+                sqlDB.close();
+                btnSelect.callOnClick();
+                Toast.makeText(getApplicationContext(), "입력됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;",null);
+
+                String strNames = "그룹 이름" + "\r\n" + "--------" + "\r\n";
+                String strCounts = "인원" + "\r\n" + "--------" + "\r\n";
+
+                while(cursor.moveToNext()) {
+                    strNames += cursor.getString(0)+"\r\n";
+                    strCounts += cursor.getString(1)+"\r\n";
+                }
+                edtNameResult.setText(strNames);
+                edtCountResult.setText(strCounts);
+
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("UPDATE groupTBL SET gNumber = " + edtCount.getText().toString() + " WHERE gName = '" + edtName.getText().toString() + "';");
+                sqlDB.close();
+                btnSelect.callOnClick();
+                Toast.makeText(getApplicationContext(), "수정됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("DELETE FROM groupTBL WHERE gName = '" + edtName.getText().toString() + "';");
+                sqlDB.close();
+                btnSelect.callOnClick();
+                Toast.makeText(getApplicationContext(), "삭제됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context) {
+            super(context,"groupDB",null,1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE groupTBL ( gName CHAR(20) PRIMARY KEY,gNumber Integer);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS groupTBL");
+            onCreate(db);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
